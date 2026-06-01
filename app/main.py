8b8af74b7299d1649fe930453e2f9486a4c958f2
Maxim Silkin <silkin619@gmail.com>
2026-06-01 14:32:17 +0300

    new branch

--- app/main.py
+++ app/main.py
@@ -1,6 +1,5 @@
 class Animal:
-    def __init__(self,
-                 name: str,
+    def __init__(self, name: str,
                  appetite: int,
                  is_hungry: bool = True) -> None:
         self.name = name
